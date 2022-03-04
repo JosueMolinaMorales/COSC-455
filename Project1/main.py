ed0ac784b47e75d05283567faf70b55fde53ee5d
Josue Morales <molinajosue92@hotmail.com>
2022-03-04 16:52:36 -0500

    Updates to project

--- Project1/main.py
+++ Project1/main.py
@@ -1,32 +1,47 @@
-
 OPERATORS = ["<", "=<", "=", "!=", ">=", ">", "+", "-", "*", "/"]
-OTHER = ["_", "(", ")", ":", ";"]
+OTHER = ["(", ")", ":", ";"]
 KEY_WORDS = ["program", "end", "bool", "int", "while", "do", "od", "print", "false", "true", "not", "and", "or", "if", "fi", "then", "else"]
-alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-
-with open("./examples/ab.txt") as file:
-    chars = [char for char in file.read()]
-    print(chars)
+ALPHA = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+WHITESPACE = [" ", "\n"]
 
 def tokenize(buffer: list):
     tokens = []
     string = ""
-    for char in buffer:
-        if char in alpha:
-            string += char
-        elif char == " " or char == "\n":
-            tokens.append(string)
-            string = ""
-    print(tokens)
+    i = 0
+    while i < len(buffer):
+        if buffer[i] in WHITESPACE:
+            i = ignoreWhiteSpace(buffer, i)
+            continue
+        if buffer[i] in ALPHA:
+            string, i = readAlpha(buffer, i)
 
-def ignoreWhiteSpace(buffer: list):
-    char = buffer[0]
-    i = 1
-    while ord(char) in [3, 9, 10, 11, 32]:
-        buffer.pop(i)
-        print("space")
-        char = buffer[i]
-    return buffer
+def readAlpha(buffer: list, curr_index: int):
+    '''
+    Allowed characters in an ID/Keyword are ALPHA, KEYWORKS, "_" and DIGIT
+    MUST BEGIN WITH A LETTER
+    '''
+    if buffer[curr_index] not in ALPHA:
+        raise RuntimeError("First character must be a letter")
+    token = ""
+    value = ""
+    charCount = curr_index
+    while curr_index < len(buffer):
+        token += buffer[curr_index]
+        curr_index += 1
+        if buffer[curr_index] in WHITESPACE:
+            curr_index = ignoreWhiteSpace(buffer, curr_index)
+            return token, curr_index
+        if buffer[curr_index] in OPERATORS:
+            return token, curr_index
+        if buffer[curr_index] != "_":
+            return "ERROR"
+        
+
+def ignoreWhiteSpace(buffer: list, curr_index: int):
+    while curr_index < len(buffer):
+        if buffer[curr_index] in WHITESPACE:
+            curr_index += 1
+    return curr_index
 
 def kindOfToken(token:str):
     if token in KEY_WORDS or token in OPERATORS or token in OTHER:
@@ -34,5 +49,14 @@ def kindOfToken(token:str):
     if token.isdigit():
         return "NUM"
     return "ID"
-        
-tokenize(chars)
+
+def main():
+    lineCount = 0
+    with open("./examples/if.txt") as file:
+        for line in file.readlines():
+            lineCount += 1
+            lineArr = [char for char in line]
+            tokenize(lineArr)
+
+if __name__ == "__main__":
+    main()
